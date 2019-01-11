bcb55fb9d5d9f93213f97ba4ac324ad619a6bf83
szapp <dev.szapp@gmail.com>
2019-01-11 17:32:22 +0100

    Initialize Workaround patch

---
+++ Ninja/Workaround/Content/NinjaInit.d
@@ -0,0 +1,30 @@
+/*
+ * Call this function (from the function "Ninja_Workaround_Init" below) to initialize LeGo packages.
+ *
+ * It ensures that all necessary LeGo packages will be loaded without breaking already loaded LeGo packages.
+ *
+ * Caution: When re-using this function elsewhere, it is important to rename it to prevent clashes!
+ * Each Ninja patch that needs it, has to have their own function with a unique name. Otherwise they cannot be stacked.
+ *
+ * Do not modify this function in any way!
+ */
+func void Ninja_Workaround_MergeLeGoFlags(var int flags) {
+    const int legoInit    = -1; // Prior initialization state
+    const int initialized =  0; // Once per session
+    var   int loaded;           // Once per game save
+    legoInit    = LeGo_MergeFlags(flags, legoInit, initialized, loaded);
+    initialized = 1;
+    loaded      = 1;
+};
+
+
+/*
+ * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
+ */
+func void Ninja_Workaround_Init() {
+    // Wrapper for "LeGo_Init" to ensure correct LeGo initialization without breaking the mod
+    Ninja_Workaround_MergeLeGoFlags(LeGo_ConsoleCommands);
+
+    CC_CallInit();
+    CC_ToggleToldInit();
+};
---
+++ README_Workaround.md
@@ -0,0 +1,26 @@
+Workaround
+==========
+
+Forked from https://github.com/szapp/NinjaPatchTemplate
+
+
+Workaround patch for Gothic and Gothic 2 NotR
+
+This patch provides various workarounds to story bugs in mods. It is especially useful to introduce workarounds to
+common story bugs in mods (e.g. close a dead quest, reset a dialog that was spoken too early, change the value of a
+variable, etc.). Until the mod team can provide a fix, this patch is a great temporary solution for critical bugs.
+
+
+Console commands:
+
+DIA TOGGLE [C_INFO]                                 -- Toggle told-status of C_Info dialog
+CALL LOG_SETTOPICSTATUS [TOPICNAME] LOG_SUCCESS     -- Set quest to successful (might not suffice in all cases)
+CALL SETI "[VARNAME]" [NEWVALUE]                    -- Set integer variable to a new value
+CALL [FUNCTION]                                     -- Call any Daedalus or external function, for more info visit
+                                                       https://forum.worldofplayers.de/forum/threads/?p=25621202
+
+
+Patch includes Ikarus and LeGo (and various scripts written by mud-freak).
+
+
+Ninja <http://tiny.cc/GothicNinja>
